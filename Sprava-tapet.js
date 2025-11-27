1bdcf426671882a648e9be52a6bfa944ec213358
jirka22med <153131598+jirka22med@users.noreply.github.com>
2025-11-27 08:00:54 +0100

    Fix typo in background image container class name

---
+++ Sprava-tapet.js
@@ -0,0 +1,11 @@
+function setBackgroundForDevice() {
+    const deviceInfo = detectDeviceType();
+    const backgrounds = {
+        desktop: 'https://img41.rajce.idnes.cz/d4102/19/19244/19244630_db82ad174937335b1a151341387b7af2/images/image_1920x1080_2.jpg?ver=0',
+        infinix: 'https://img41.rajce.idnes.cz/d4102/19/19244/19244630_db82ad174937335b1a151341387b7af2/images/image_1024x1792.jpg?ver=0'
+    };
+    let backgroundUrl = deviceInfo.isInfinixNote30 ? backgrounds.infinix : backgrounds.desktop;
+    const bgContainer = document.querySelector('.background-image-container img');
+    if (bgContainer) bgContainer.src = backgroundUrl;
+    localStorage.setItem('background_url', backgroundUrl);
+}
